b12aab693aa9fa81ab189170fd60d3df0258bc64
martin.galajda <galajda.mato@gmail.com>
2016-12-13 09:12:00 +0100

    Fix

--- app/app.component.ts
+++ app/app.component.ts
@@ -34,7 +34,7 @@ export class AppComponent implements OnInit {
 	}
 
 	goToProjects(): void {
-		this.router.navigate(['/projects-list']);
+		this.router.navigate(['/project-list']);
 	}
 
 	goToConverters(): void {
